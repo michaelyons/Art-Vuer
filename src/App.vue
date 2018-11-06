b028d307eaeac4d2d06bedd5ed821c1e48c40101
Michael J. Lyons <mikelyons7@me.com>
2018-11-06 13:38:22 -0700

    Add new img for welcome page

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,16 @@
 
 <style>
 html {
-  background: #cc95c0; /* fallback for old browsers */
+  background: #ffefba; /* fallback for old browsers */
   background: -webkit-linear-gradient(
     to right,
-    #7aa1d2,
-    #dbd4b4,
-    #cc95c0
+    #ffffff,
+    #ffefba
   ); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
     to right,
-    #7aa1d2,
-    #dbd4b4,
-    #cc95c0
+    #ffffff,
+    #ffefba
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 #app {
